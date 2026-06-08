c87e6430f98df4d54414c3caaa3bfbf6ebcc461d
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-06-08 14:52:37 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Courses from "./pages/Courses";
+import Qcsp from "./pages/courses/Qcsp";
+import Contact from "./pages/Contact";
+import Ceh from "./pages/courses/Ceh";
 
 const App = () => {
   return (
@@ -13,7 +15,9 @@ const App = () => {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
-            <Route path="courses" element={<Courses />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="courses/QCSP" element={<Qcsp />} />
+            <Route path="courses/CEH" element={<Ceh />} />
           </Route>
         </Routes>
       </BrowserRouter>
