d033def31d00a0a73fb8077da068e2e79e8d9c16
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-06-08 14:53:55 +0530

    logo file changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import logo from "../assets/logo/QNAYDS_LOGO.webp";
+import logo from "../assets/logo/QNAYDS_LOGO.png";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
