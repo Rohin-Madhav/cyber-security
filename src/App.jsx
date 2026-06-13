dac72146ded8093388b2696b0058ef1a587a657f
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-06-13 13:16:33 +0530

    route change

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
             <Route path="courses/QCSP" element={<Qcsp />} />
-            <Route path="courses/CEH" element={<Ceh />} />
+            <Route path="courses/JEH" element={<Ceh />} />
           </Route>
         </Routes>
       </BrowserRouter>
